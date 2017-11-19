f67bf7120ab33ee41dc965916444b1bdddb17f08
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-11-19 12:17:23 +0000

    working though not satisfactory for me

--- amd.js
+++ amd.js
@@ -16,7 +16,7 @@ var currentMod = {
 
 function getModule(name) {
   let gM = new Promise((success, reject) => {
-    //if (name in defineCache) success(defineCache[name]);
+    if (name in defineCache) success(defineCache[name]);
 
     var module = {
       name: name,    // for debugging only
@@ -32,15 +32,12 @@ function getModule(name) {
        * update this object when it finishes loading.
        */
       currentMod = module;
-      //console.log("backgroundReadFile callback: finished reading file "+name+" now is currentMod: "+inspect(currentMod));
       /*jslint evil: true */ // https://github.com/jamesallardice/jslint-error-explanations/blob/master/message-articles/function-constructor.md
       new Function("", code)();  // Evaluate it!
       success(module);
     }, (err) => {
-      console.log("rejecting ... "+name);
       reject(err);
     });
-    // return module;
   });
   return gM;
 }
@@ -67,31 +64,22 @@ function define(depNames, moduleFunction) {
          * when the last dependency has finished loading.
          */
 
-      //console.log("whenDepsLoaded. myMod = "+inspect(myMod));
       var args = modules.map(function(m) { 
         return m.exports; 
       });
-      //console.log("whenDepsLoaded: calling with args:\n  "+inspect(args));
       var exports = moduleFunction.apply(null, args);
       if (myMod) {
         myMod.exports = exports;
         myMod.loaded = true;
         myMod.onLoad.forEach(function(f) { f(); }); // Call whenDepsLoaded for all the modules which depend on this module
-        // console.log("whenDepsLoaded after: myMod = "+inspect(myMod, {depth: null})+"\n   depNames = "+depNames);
-        // console.log("whenDepsLoaded after. All deps read. Array modules is:\n  "+inspect(modules, {depth: null}));
       }
     }
 
     modules.forEach(function(mod) {
       if (!mod.loaded)  // whenDepsLoaded is added to the onLoad array of all dependencies that are not yet loaded.
         mod.onLoad.push(whenDepsLoaded); // I believe closure here plays an important role
-      // console.log("mod.onLoad: "+inspect(mod.onLoad));
     });
     whenDepsLoaded();
-    // var exports = moduleFunction.apply(null, args);
-    // myMod.loaded = true;
-    // myMod.exports = exports;
-    //  myMod.onLoad.forEach(function(f) { f(); }); // Call whenDepsLoaded for all the modules which depend on this module
   }).catch((error) => console.log("Ufff!!!! myMod = "+inspect(myMod)+" Error: "+error));
 }
 /* function define(module dependencies list, callback) */
